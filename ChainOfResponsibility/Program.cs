6b36c42b3b591b502791aa1f38ee85e6dd6cc8be
TengDk7 <2733934409@qq.com>
2020-06-29 22:28:42 +0800

    职责链模式

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -10,6 +10,24 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
+            RelatedPerson p1, p2, p3, p4;
+            p1 = new Classsmate();
+            p2 = new Friend();
+            p3 = new Parent();
+            p4 = new Bank();
+
+            p1.SetNextPerson(p2);
+            p2.SetNextPerson(p3);
+            p3.SetNextPerson(p4);
+
+            p1.LendMoney(999);
+            Console.WriteLine();
+            p1.LendMoney(9999);
+            Console.WriteLine();
+            p1.LendMoney(99999);
+            Console.WriteLine();
+            p1.LendMoney(999999);
+            Console.ReadKey();
         }
     }
 }
---
+++ ChainOfResponsibility/RelatedPerson.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    abstract class RelatedPerson
+    {
+        protected RelatedPerson nextperson;
+        public void SetNextPerson(RelatedPerson person)
+        {
+            nextperson = person;
+        }
+        public abstract void LendMoney(int money);
+    }
+    class Classsmate : RelatedPerson
+    {
+        public override void LendMoney(int money)
+        {
+            if(money<1000)
+            {
+                Console.WriteLine("同学可以借给你");
+            }
+            else
+            {
+                Console.WriteLine("同学没钱");
+                nextperson.LendMoney(money);
+            }
+        }
+    }
+    class Friend : RelatedPerson
+    {
+        public override void LendMoney(int money)
+        {
+            if(money<10000)
+            {
+                Console.WriteLine("朋友可以借给你");
+            }
+            else
+            {
+                Console.WriteLine("朋友没钱");
+                nextperson.LendMoney(money);
+            }
+        }
+    }
+    class Parent : RelatedPerson
+    {
+        public override void LendMoney(int money)
+        {
+            if(money<100000)
+            {
+                Console.WriteLine("父母可以借给你");
+            }
+            else
+            {
+                Console.WriteLine("父母没钱");
+                nextperson.LendMoney(money);
+            }
+        }
+    }
+    class Bank : RelatedPerson
+    {
+        public override void LendMoney(int money)
+        {
+            Console.WriteLine("银行可以借给你");
+        }
+    }
+}
