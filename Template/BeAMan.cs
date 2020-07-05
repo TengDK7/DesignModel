0562b21ac004ced3dc3c41db55037f0ef3161955
TengDk7 <2733934409@qq.com>
2020-07-05 08:23:37 +0800

    模板方法模式

---
+++ Template/BeAMan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    abstract class BeAMan
+    {
+        public void GroupUp()
+        {
+            Console.WriteLine("首先需要先长大");
+        }
+        public void LearnKnowledge()
+        {
+            Console.WriteLine("学习基础知识");
+        }
+        public abstract void LearnTechnology();
+        public void FeedByself()
+        {
+            Console.WriteLine("可以自立更生");
+        }
+        public void TheWayToBeAMan()
+        {
+            GroupUp();
+            LearnKnowledge();
+            LearnTechnology();
+            FeedByself();
+        }
+    }
+    class BeAScientist : BeAMan
+    {
+        public override void LearnTechnology()
+        {
+            Console.WriteLine("钻研科学知识,成为科学家");
+        }
+    }
+    class BeAnArtist : BeAMan
+    {
+        public override void LearnTechnology()
+        {
+            Console.WriteLine("积累文学素养，成为文学家");
+        }
+    }
+    class BeALoser : BeAMan
+    {
+        public override void LearnTechnology()
+        {
+            Console.WriteLine("啥技能也没学好,成为一个失败者");
+        }
+    }
+}
--- Template/Program.cs
+++ Template/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
 
 namespace Template
 {
@@ -10,6 +8,17 @@ namespace Template
     {
         static void Main(string[] args)
         {
+            var name = ConfigurationManager.AppSettings["ManStyle"];
+            BeAMan person = (BeAMan)Assembly.Load("Template").CreateInstance(name);
+            person.TheWayToBeAMan();
+            Console.WriteLine();
+            person = new BeAScientist();
+            person.TheWayToBeAMan();
+            Console.WriteLine();
+            person = new BeAnArtist();
+            person.TheWayToBeAMan();
+            Console.WriteLine();
+            Console.Read();
         }
     }
 }
